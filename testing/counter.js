9c3cfe1c24bda1c8ab9097b4b34cd9902a7f1f78
ChandanPk <chandanpk9@gmail.com>
2021-04-16 20:24:08 +0530

    Added a counter button

---
+++ testing/counter.js
@@ -0,0 +1,9 @@
+let num = 0;
+document.querySelector('button').addEventListener('click', ()=> {
+    num = num + 1;
+    document.querySelector('em').innerHTML = num;
+});
+
+const counter = ()=> {
+
+}
