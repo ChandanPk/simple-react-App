d4651f292d932ccf8d358a6ec73f4e02371944aa
ChandanPk <chandanpk9@gmail.com>
2021-04-24 12:44:53 +0530

    fixed some issue

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -1,50 +1,90 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 const SearchField = () => {
-    console.log('bitch')
-    var products = [];
-    const [apiProducts, setApi] = useState();
+    const [filteredItem, setFilter] = useState(null);
+    const [apiProducts, setProducts] = useState([]);
+    const [searchValue, setSearch] = useState('');
 
-    
 
-    
 
-    fetch("https://fakestoreapi.com/products/")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
+    useEffect(() => {
 
-    // for(var i=0; i<json.products.length; i++){
-        //     json.products.map(product => {
-        //         console.log(json.products[i].name);
-        //     })
-    // console.log(products)
-    
+        setTimeout(() => {
+            fetch("https://fakestoreapi.com/products/")
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    setProducts(data);   
+                    filterFunction();    
+                })
+        }, 2000);
 
-    const [searchValue, setSearch] = useState('')
-    
-        const handleInput = (e)=> {
+    }, [])
+
+
+    // var isProduct = "";
+
+    // if (apiProducts) {
+    //     isProduct = true
+    // } else {
+    //     isProduct = false
+    // }
+
+    // const isProduct = apiProducts ? true : false;
+
+
+    const handleInput = (e) => {
         setSearch(e.target.value);
+      
+
+    }
+
+    const filterFunction = ()=> {
+        let reFiltered = apiProducts.filter(product => product.title.includes(searchValue));
+        setFilter(reFiltered)
+        
+ }
+
     
-}
-        const filteredItem = products.filter(product=> product.includes(searchValue));
-        const buttonVisibility = searchValue.length > 0;
+    
+    // if (isProduct){
+    //  var filteredItem = apiProducts.filter(product => true)
+    // }
 
-    return ( 
+
+    const buttonVisibility = searchValue.length > 0;
+
+    return (
         <div className="searchField">
-        <div className="home">
+            <div className="home">
 
-            <input type="text" name="values" placeholder="Seach for products" onChange={(e)=> handleInput(e)}/>
-            {buttonVisibility && <button>clear</button>}
+                <input type="text" name="values" placeholder="Seach for products" onChange={(e) => handleInput(e)} />
+                {buttonVisibility && <button>clear</button>}
 
-            <div className="products"><em>{products.map(value=> {
-                return <ul key={value}><li>{value}</li></ul>
-            })}</em></div>
+                {filteredItem ? <div className="products"><em>{filteredItem.map(value => {
+                    return <p>loll</p> })}</em></div> : <p>Loding...</p>}
 
+            </div>
         </div>
-        </div>
-     );
+    );
 }
- 
+
 export default SearchField;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
